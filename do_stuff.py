e4fe9a04a853fa319fc3d72e258cc50bb9a20a04
Empujador <b.schaedel@xuffix.com>
2014-02-27 12:13:16 +0100

    clean scripts set reanimate to sani

--- do_stuff.py
+++ do_stuff.py
@@ -3,19 +3,15 @@ import random
 import subprocess
  
 
+class Sound():
 
-class Sound() :
-
-	def play_from_dir(self, folderName):
-
-
-		baseDir = os.path.dirname(os.path.realpath(__file__)) + "/sounds/" + folderName	
-		audio = baseDir + "/" + random.choice(os.listdir(baseDir)) 
-		subprocess.call(["mpg123",audio])
+    def play_from_dir(self, folderName):
+        baseDir = os.path.dirname(os.path.realpath(__file__)) + "/sounds/" + folderName
+        audio = baseDir + "/" + random.choice(os.listdir(baseDir))
+        subprocess.call(["mplayer",audio])
 
 
-	def play_misc(self, fileName):
-
-                baseDir = os.path.dirname(os.path.realpath(__file__)) + "/sounds/misc"
-                audio = baseDir + "/" +fileName
-                subprocess.call(["mpg123",audio])
+    def play_misc(self, fileName):
+        baseDir = os.path.dirname(os.path.realpath(__file__)) + "/sounds/misc"
+        audio = baseDir + "/" + fileName
+        subprocess.call(["mplayer",audio])
