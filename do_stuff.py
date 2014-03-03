36a04e1de932f96929f0d4338442be30122251b6
Reimerei <git@bjrm.de>
2014-03-03 12:42:08 +0100

    more logging and some extra sounds

--- do_stuff.py
+++ do_stuff.py
@@ -1,20 +1,19 @@
 import os
 import random 
 import subprocess
- 
+
+DEVNULL = open(os.devnull, 'w')
 
 
-class Sound() :
+def play_misc(fileName):
 
-	def play_from_dir(self, folderName):
-
-		baseDir = os.path.dirname(os.path.realpath(__file__)) + "/sounds/" + folderName	
-		audio = baseDir + "/" + random.choice(os.listdir(baseDir)) 
-		subprocess.call(["mplayer",audio])
+        baseDir = os.path.dirname(os.path.realpath(__file__)) + "/sounds/misc"
+        audio = baseDir + "/" +fileName
+        subprocess.call(["mplayer",audio],stdout=DEVNULL, stderr=subprocess.STDOUT)
 
 
-	def play_misc(self, fileName):
+def play_from_dir(folderName):
 
-        	baseDir = os.path.dirname(os.path.realpath(__file__)) + "/sounds/misc"
-        	audio = baseDir + "/" +fileName
-        	subprocess.call(["mplayer",audio])
+    baseDir = os.path.dirname(os.path.realpath(__file__)) + "/sounds/" + folderName
+    audio = baseDir + "/" + random.choice(os.listdir(baseDir))
+    subprocess.call(["mplayer",audio,"&>/dev/null"], stdout=DEVNULL, stderr=subprocess.STDOUT)
